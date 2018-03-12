432e37506a88c6a273294329b79b5c5a19e6bc75
Christian Paun <christianpaun@gmail.com>
2018-03-12 22:36:41 +0000

    added delete & put routes

--- routes/properties_routes.js
+++ routes/properties_routes.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 require('../models/Property');
 var Property = mongoose.model('Property');
 
-/* GET users listing. */
+/* GET all properties listing. */
+
+//find with query '?'
 router.get('/', function (req, res, next) {
 
   Property.find(req.query, (err, r) => {
@@ -13,23 +15,44 @@ router.get('/', function (req, res, next) {
   })
 });
 
+//get one property by id
+
 router.get('/:_id', function (req, res, next) {
-console.log(req.params);
-  Property.findOne(req.params, (err,r)=>{
+  console.log(req.params);
+  Property.findOne(req.params, (err, r) => {
     res.send(r);
   });
 });
 
+//delete
 router.delete('/:_id', function (req, res, next) {
-  
-  Property.findByIdAndRemove(req.params._id, function(err,data)
-{
-    if(!err){
-        console.log("Deleted");
-        res.send();
+  Property.findByIdAndRemove(req.params._id, function (err, data) {
+    if (!err) {
+      console.log("Deleted");
+      res.send();
     }
-});
-  
   });
+});
+
+//create
+
+router.post('/', function (req, res, next) {
+  console.log(typeof(req.body));
+  const newObj = new Property(req.body);
+  newObj.save(err=>{
+  if (err) return res.status(500).send(err);
+  return res.status(200).send(newObj);
+  });
+ 
+});
+
+//update
+router.put('/:_id', function (req, res, next) {
+  //console.log(req.params._id);
+  Property.findByIdAndUpdate(req.params._id, req.body, {new: true},(err, todo) => {
+    if (err) return res.status(500).send(err);
+    return res.send(todo);
+});
+});
 
 module.exports = router;
